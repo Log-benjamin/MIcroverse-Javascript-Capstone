1d543092cd6065c387ff843684e4e3cc791eebde
Abbas Aljuaifari <fares.aljuaifari@gmail.com>
2023-07-06 16:59:12 +0530

    Import popup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import Logo from './asset/logo-img.gif';
 import {
-  logoContainer, allCountriesbtn, Americasbtn, Asiabtn, Africabtn, Europebtn,
+  cardContainer, searchBtn, countryInp, logoContainer, allCountriesbtn, Americasbtn, Asiabtn,
+  Africabtn, Europebtn,
 } from './modules/DomValues.js';
 import loadCountries from './modules/populate.js';
+import { addComment } from './modules/submitComment.js';
 
 const myIcon = new Image();
 myIcon.src = Logo;
@@ -20,3 +22,14 @@ Africabtn.addEventListener('click', () => loadCountries('Africa'));
 Asiabtn.addEventListener('click', () => loadCountries('Asia'));
 
 Americasbtn.addEventListener('click', () => loadCountries('Americas'));
+
+cardContainer.addEventListener('click', (e) => {
+  if (e.target.className === 'commentBtn') {
+    searchandFind(e.target.id);
+  }
+
+  if (e.target.id === 'remove') {
+    loadCountries('allcountries');
+  }
+
+});
