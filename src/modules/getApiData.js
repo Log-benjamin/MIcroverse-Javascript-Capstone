8918edd408894c746541fc30331123a01e510d69
binyamT <binyampowerhc@gmail.com>
2023-07-06 12:42:46 +0300

    add external API for the Project

---
+++ src/modules/getApiData.js
@@ -0,0 +1,5 @@
+const baseURL = 'https://restcountries.com/v3.1/all';
+const involvmentApiLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vaUjrjm1SfSLzppPGOuv/likes`;
+const involvmentApiComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vaUjrjm1SfSLzppPGOuv/comments`;
+
+export { baseURL, involvmentApiLikes,involvmentApiComments };
