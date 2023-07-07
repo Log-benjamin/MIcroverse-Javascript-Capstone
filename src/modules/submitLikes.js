e257f7b048b2822adf542e21321c092122b4eb89
binyamT <binyampowerhc@gmail.com>
2023-07-07 11:26:08 +0300

    add filesto display likes on home page

---
+++ src/modules/submitLikes.js
@@ -0,0 +1,35 @@
+import { involvmentApiLikes } from './getApiData.js';
+
+const addLike = async (countryName) => {
+  try {
+    await fetch(involvmentApiLikes, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        item_id: countryName,
+      }),
+    });
+    return 'Recived all data';
+  } catch (error) {
+    return `Adding Likes error:${error}`;
+  }
+};
+
+const getLikeData = async () => {
+  try {
+    const response = await fetch(involvmentApiLikes, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export { addLike, getLikeData };
